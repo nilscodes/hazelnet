1d3ca4b7b9f941d95a6aa21ba8b69941d108821a
Nils Peuser <nils.peuser@igrafx.com>
2022-04-20 19:07:50 -0700

    Added capabilities to reset command permissions of an individual server.

--- hazelnet-discordbot/schedules/reconfigurecommands.js
+++ hazelnet-discordbot/schedules/reconfigurecommands.js
@@ -3,19 +3,27 @@ const commandregistration = require('../utility/commandregistration');
 const commandPermissions = require('../utility/commandpermissions');
 
 module.exports = {
-  cron: '*/5 * * * *',
+  cron: '*/15 * * * *',
   async execute(client) {
     client.logger.info('Checking if all servers need their commands and permissions updated...');
     try {
       const resetAllPermissions = await client.services.globalsettings.getGlobalSetting('RESET_ALL_COMMANDS') === 'true';
+      await client.services.globalsettings.updateGlobalSetting('RESET_ALL_COMMANDS', 'false');
+      const resetSpecificGuildsString = await client.services.globalsettings.getGlobalSetting('RESET_COMMANDS_FOR_GUILDS');
+      const guildsToReset = (resetSpecificGuildsString.length && resetSpecificGuildsString?.split(',')) ?? [];
+      await client.services.globalsettings.updateGlobalSetting('RESET_COMMANDS_FOR_GUILDS', '');
       if (resetAllPermissions) {
-        client.logger.info('Command and permissions on all servers need to be reset. Performing task now.');
+        if (guildsToReset.length) {
+          client.logger.info(`Command and permissions on these servers need to be reset: ${guildsToReset.join(', ')}. Performing task now.`);
+        } else {
+          client.logger.info('Command and permissions on all servers need to be reset. Performing task now.');
+        }
         const allServers = await client.services.discordserver.getAllDiscordServers();
         for (let i = 0; i < allServers.length; i += 1) {
           const discordServer = allServers[i];
           try {
             const enabledCommands = discordServer.settings?.ENABLED_COMMAND_TAGS;
-            if (enabledCommands?.length) {
+            if (enabledCommands?.length && (!guildsToReset.length || guildsToReset.includes(discordServer.guildId))) {
               await commandregistration.registerMainCommands(enabledCommands.split(','), client, discordServer.guildId);
               await commandPermissions.setSlashCommandPermissions(client, discordServer.guildId, discordServer);
             }
@@ -23,7 +31,6 @@ module.exports = {
             client.logger.error({ msg: `Failed to reset commands and permissions for ${discordServer.guildName} (${discordServer.guildId})` });
           }
         }
-        await client.services.globalsettings.updateGlobalSetting('RESET_ALL_COMMANDS', 'false');
       } else {
         client.logger.info('... No command and permission reset required.');
       }
