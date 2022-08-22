3ecf664e666f4711f11057b02c29196eb7e665bb
Nils Peuser <nils.peuser@igrafx.com>
2022-08-21 22:44:55 -0700

    Better error handling for regex expression errors in filters.
    Also disallow removing and unlinking from blacklisted accounts.

--- hazelnet-discordbot/commands/verify/unlink.js
+++ hazelnet-discordbot/commands/verify/unlink.js
@@ -6,16 +6,22 @@ module.exports = {
     try {
       await interaction.deferReply({ ephemeral: true });
       const discordServer = await interaction.client.services.discordserver.getDiscordServer(interaction.guild.id);
-      const useLocale = discordServer.getBotLanguage();
+      const locale = discordServer.getBotLanguage();
       const externalAccount = await interaction.client.services.externalaccounts.createOrUpdateExternalDiscordAccount(interaction.user.id, interaction.user.tag);
-      const currentMembers = await interaction.client.services.discordserver.listExternalAccounts(interaction.guild.id);
-      const currentMemberData = currentMembers.find((member) => member.externalAccountId === externalAccount.id);
-      if (currentMemberData) {
-        await interaction.client.services.discordserver.disconnectExternalAccount(interaction.guild.id, currentMemberData.externalAccountId);
-        const embed = embedBuilder.buildForUserWithAd(externalAccount, discordServer, i18n.__({ phrase: 'verify.unlink.messageTitle', locale: useLocale }), i18n.__({ phrase: 'verify.unlink.success', locale: useLocale }), 'verify-unlink');
-        await interaction.editReply({ embeds: [embed], ephemeral: true });
+      const mainAccount = await interaction.client.services.externalaccounts.getAccountForExternalAccount(externalAccount.id);
+      if (mainAccount.settings?.BLACKLISTED !== 'true') {
+        const currentMembers = await interaction.client.services.discordserver.listExternalAccounts(interaction.guild.id);
+        const currentMemberData = currentMembers.find((member) => member.externalAccountId === externalAccount.id);
+        if (currentMemberData) {
+          await interaction.client.services.discordserver.disconnectExternalAccount(interaction.guild.id, currentMemberData.externalAccountId);
+          const embed = embedBuilder.buildForUserWithAd(externalAccount, discordServer, i18n.__({ phrase: 'verify.unlink.messageTitle', locale }), i18n.__({ phrase: 'verify.unlink.success', locale }), 'verify-unlink');
+          await interaction.editReply({ embeds: [embed], ephemeral: true });
+        } else {
+          const embed = embedBuilder.buildForUser(discordServer, i18n.__({ phrase: 'verify.unlink.messageTitle', locale }), i18n.__({ phrase: 'verify.unlink.notLinked', locale }), 'verify-unlink');
+          await interaction.editReply({ embeds: [embed], ephemeral: true });
+        }
       } else {
-        const embed = embedBuilder.buildForUser(discordServer, i18n.__({ phrase: 'verify.unlink.messageTitle', locale: useLocale }), i18n.__({ phrase: 'verify.unlink.notLinked', locale: useLocale }), 'verify-unlink');
+        const embed = embedBuilder.buildForUser(discordServer, i18n.__({ phrase: 'verify.add.blacklistedTitle', locale }), i18n.__({ phrase: 'verify.add.blacklisted', locale }), 'verify-link');
         await interaction.editReply({ embeds: [embed], ephemeral: true });
       }
     } catch (error) {
