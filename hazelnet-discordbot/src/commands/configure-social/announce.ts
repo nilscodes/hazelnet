87967f451875c25272fb6570700fd16a8feda0fb
Nils Peuser <nils.peuser@igrafx.com>
2022-12-27 21:06:38 -0800

    Removed token policies check copy and pasted from verify-announce command

--- hazelnet-discordbot/src/commands/configure-social/announce.ts
+++ hazelnet-discordbot/src/commands/configure-social/announce.ts
@@ -27,59 +27,54 @@ export default <SocialAnnounceCommand> {
       const discordServer = await interaction.client.services.discordserver.getDiscordServer(interaction.guild!.id);
       const tokenPolicies = await interaction.client.services.discordserver.listTokenPolicies(interaction.guild!.id);
       const locale = discordServer.getBotLanguage();
-      if (tokenPolicies.length) {
-        if (announceChannel
-          && (announceChannel.type === ChannelType.GuildText || announceChannel.type === ChannelType.GuildAnnouncement)) {
-          const guildChannel = announceChannel as GuildChannel;
-          const announceChannelPermissions = guildChannel.permissionsFor(interaction.client.application!.id);
-          if (announceChannelPermissions) {
-            if (announceChannelPermissions.has(PermissionsBitField.Flags.SendMessages) && announceChannelPermissions.has(PermissionsBitField.Flags.ViewChannel) && announceChannelPermissions.has(PermissionsBitField.Flags.EmbedLinks)) {
+      if (announceChannel
+        && (announceChannel.type === ChannelType.GuildText || announceChannel.type === ChannelType.GuildAnnouncement)) {
+        const guildChannel = announceChannel as GuildChannel;
+        const announceChannelPermissions = guildChannel.permissionsFor(interaction.client.application!.id);
+        if (announceChannelPermissions) {
+          if (announceChannelPermissions.has(PermissionsBitField.Flags.SendMessages) && announceChannelPermissions.has(PermissionsBitField.Flags.ViewChannel) && announceChannelPermissions.has(PermissionsBitField.Flags.EmbedLinks)) {
 
-              const embed = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.purpose', locale }, { channel: announceChannel.id }), 'configure-social-announce');
-              await interaction.editReply({ embeds: [embed] });
+            const embed = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.purpose', locale }, { channel: announceChannel.id }), 'configure-social-announce');
+            await interaction.editReply({ embeds: [embed] });
 
-              const collector = interaction.channel!.createMessageCollector({
-                filter: (message) => message.author.id === interaction.user.id,
-                time: 5 * 60000,
-                dispose: true,
-                max: 1,
-              });
+            const collector = interaction.channel!.createMessageCollector({
+              filter: (message) => message.author.id === interaction.user.id,
+              time: 5 * 60000,
+              dispose: true,
+              max: 1,
+            });
 
-              collector.on('end', async (collected) => {
-                if (collected.size > 0) {
-                  const announcementText = collected.at(0)!.content;
-                  this.cache!.set(`${interaction.guild!.id}-${interaction.user.id}`, {
-                    announceChannelId: announceChannel.id,
-                    title,
-                    announcementText,
-                  });
+            collector.on('end', async (collected) => {
+              if (collected.size > 0) {
+                const announcementText = collected.at(0)!.content;
+                this.cache!.set(`${interaction.guild!.id}-${interaction.user.id}`, {
+                  announceChannelId: announceChannel.id,
+                  title,
+                  announcementText,
+                });
 
-                  const components = [
-                    new ActionRowBuilder()
-                      .addComponents(
-                        new ButtonBuilder()
-                          .setCustomId('configure-social/announce/confirm')
-                          .setLabel(i18n.__({ phrase: 'configure.social.announce.publish', locale }))
-                          .setStyle(ButtonStyle.Primary),
-                      ),
-                  ] as ActionRowBuilder<MessageActionRowComponentBuilder>[];
+                const components = [
+                  new ActionRowBuilder()
+                    .addComponents(
+                      new ButtonBuilder()
+                        .setCustomId('configure-social/announce/confirm')
+                        .setLabel(i18n.__({ phrase: 'configure.social.announce.publish', locale }))
+                        .setStyle(ButtonStyle.Primary),
+                    ),
+                ] as ActionRowBuilder<MessageActionRowComponentBuilder>[];
 
-                  const embed = embedBuilder.buildForUser(discordServer, title, announcementText);
-                  await interaction.followUp({ components, embeds: [embed], ephemeral: true });
-                }
-              });
+                const embed = embedBuilder.buildForUser(discordServer, title, announcementText);
+                await interaction.followUp({ components, embeds: [embed], ephemeral: true });
+              }
+            });
 
-            } else {
-              const embedAdmin = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.errorNoSendPermissions', locale }, { channel: announceChannel.id }), 'configure-social-announce');
-              await interaction.editReply({ embeds: [embedAdmin], components: [] });
-            }
+          } else {
+            const embedAdmin = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.errorNoSendPermissions', locale }, { channel: announceChannel.id }), 'configure-social-announce');
+            await interaction.editReply({ embeds: [embedAdmin], components: [] });
           }
-        } else {
-          const embed = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.errorWrongChannelType', locale }), 'configure-social-announce');
-          await interaction.editReply({ embeds: [embed] });
         }
       } else {
-        const embed = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.errorNoPolicies', locale }), 'configure-social-announce');
+        const embed = embedBuilder.buildForAdmin(discordServer, '/configure-social announce', i18n.__({ phrase: 'configure.social.announce.errorWrongChannelType', locale }), 'configure-social-announce');
         await interaction.editReply({ embeds: [embed] });
       }
     } catch (error) {
